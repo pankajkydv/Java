d4eccb9a44d073b45743ee9aa87121e068f22280
Pankaj Yadav <42650848+pankajkydv@users.noreply.github.com>
2020-08-24 22:19:07 +0530

    Add files via upload

--- LinkedListQues/ClientLinkedList.java
+++ LinkedListQues/ClientLinkedList.java
@@ -2,29 +2,35 @@ package LinkedListQues;
 
 public class ClientLinkedList {
 
-	public static void main(String[] args)throws Exception {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-LinkedList list=new LinkedList();
+		LinkedList list = new LinkedList();
 //list.AddLast(10);
 //list.AddLast(20);
 //list.AddLast(30);
 //list.AddLast(40);
-list.AddFirst(10);
-list.AddFirst(20);
-list.AddFirst(30);
-list.AddFirst(40);
-list.display();
-System.out.println("first data="+list.GetFirst());
-System.out.println("last data="+list.GetLast());
-System.out.println("geta data at index="+list.GetAt(0));
-//System.out.println("geta Node at index="+list.GetNodeAt(2));
-list.AddAt(50,4);
-list.display();
-System.out.println(list.RemoveFirst());
-list.display();
-System.out.println(list.RemoveLast());
-list.display();
-
+		list.AddFirst(10);
+		list.AddFirst(20);
+		list.AddFirst(30);
+		list.AddFirst(40);
+		list.AddFirst(50);
+		list.AddFirst(60);
+		list.display();
+		System.out.println("first data=" + list.GetFirst());
+		System.out.println("last data=" + list.GetLast());
+		System.out.println("geta data at index=" + list.GetAt(0));
+//System.out.println("get Node at index="+list.GetNodeAt(2));
+		list.AddAt(60, 4);
+		list.display();
+		System.out.println(list.RemoveFirst());
+		list.display();
+		System.out.println(list.RemoveLast());
+		list.display();
+		list.ReverseData();
+		list.display();
+		list.ReversePointer();
+		list.display();
+		System.out.println(list.Mid());
 
 	}
 
--- LinkedListQues/LinkedList.java
+++ LinkedListQues/LinkedList.java
@@ -1,180 +1,228 @@
 package LinkedListQues;
 
-public  class LinkedList {
-private class Node	{
-	int data;
-	Node next;
+public class LinkedList {
+	private class Node {
+		int data;
+		Node next;
 
-}
-private Node head;
-private Node tail;
-int size;
-
-public void display() {
-	System.out.println("-------------");
-	Node temp=this.head;
-	while(temp!=null) {
-		System.out.print(temp.data+" ");
-		temp=temp.next;
 	}
-	System.out.println();
-	System.out.println("-----------------");
-}
-public void AddLast(int item) {
+
+	private Node head;
+	private Node tail;
+	int size;
+
+	public void display() {
+		System.out.println("-------------");
+		Node temp = this.head;
+		while (temp != null) {
+			System.out.print(temp.data + " ");
+			temp = temp.next;
+		}
+		System.out.println();
+		System.out.println("-----------------");
+	}
+
+	public void AddLast(int item) {
 //	create new node
-	Node nn=new Node();
-	nn.data=item;
-	nn.next=null;
-	
+		Node nn = new Node();
+		nn.data = item;
+		nn.next = null;
+
 //	attach new node
-	if(this.size>=1) {
-		this.tail.next=nn;
-	}
+		if (this.size >= 1) {
+			this.tail.next = nn;
+		}
 //	update summary object(head,tail,size)
-	if(this.size==0) {
-		this.head=nn;
-		this.tail=nn;
-		this.size++;
+		if (this.size == 0) {
+			this.head = nn;
+			this.tail = nn;
+			this.size++;
+		} else {
+			this.tail = nn;
+		}
 	}
-	else {
-		this.tail=nn;
-	}
-}
-public void AddFirst(int item) {
+
+	public void AddFirst(int item) {
 //	create new node
-	Node nn=new Node();
-	nn.data=item;
-	nn.next=null;
+		Node nn = new Node();
+		nn.data = item;
+		nn.next = null;
 //	attach new node
-	if(this.size>=1) {
-		nn.next=head;
-	}
+		if (this.size >= 1) {
+			nn.next = head;
+		}
 //	update my summary  objects
-	if(this.size==0) {
-		this.head=nn;
-		this.tail=nn;
-		this.size++;
+		if (this.size == 0) {
+			this.head = nn;
+			this.tail = nn;
+			this.size++;
+		} else {
+			this.head = nn;
+			this.size++;
+		}
 	}
-	else {
-		this.head=nn;
-		this.size++;
+
+	public int GetFirst() throws Exception {
+		if (this.size == 0) {
+			throw new Exception("Linked list is empty");
+		}
+		return this.head.data;
 	}
-}
-public int GetFirst()throws Exception {
-	if(this.size==0) {
-     throw new Exception("Linked list is empty");
+
+	public int GetLast() throws Exception {
+		if (this.size == 0) {
+			throw new Exception("linked list is empty");
+		}
+		return this.tail.data;
+
 	}
-	return this.head.data;
-}
-public int GetLast()throws Exception{
-	if(this.size==0) {
-		throw new Exception("linked list is empty");
+
+	public int GetAt(int indx) throws Exception {
+		if (this.size == 0) {
+			throw new Exception("Linked list is empty");
+		}
+		if (indx < 0 || indx >= this.size) {
+			throw new Exception("index is invalid");
+		}
+		Node temp = this.head;
+		for (int i = 1; i <= indx; i++) {
+			temp = temp.next;
+		}
+		return temp.data;
+
 	}
-	return this.tail.data;
-	
-}
-public int GetAt(int indx)throws Exception {
-	if(this.size==0) {
-		throw new Exception("Linked list is empty");
+
+	private Node GetNodeAt(int index) throws Exception {
+		if (this.size == 0) {
+			throw new Exception("Linked list is empty");
+		}
+		if (index < 0 || index >= this.size) {
+			throw new Exception("index is invalid");
+		}
+		Node temp = this.head;
+		for (int i = 1; i <= index; i++) {
+			temp = temp.next;
+		}
+		return temp;
 	}
-	if(indx<0||indx>=this.size) {
-		throw new Exception("index is invalid");
+
+	public void AddAt(int item, int indx) throws Exception {
+		if (indx < 0 || indx > this.size) {
+			throw new Exception("index is invalid");
+		}
+		if (indx == 0) {
+			AddFirst(item);
+		}
+
+		else if (indx == this.size) {
+			AddLast(item);
+		} else {
+//	create new node
+			Node nn = new Node();
+			nn.data = item;
+			nn.next = null;
+//	attach my new node at index
+			Node nm1 = GetNodeAt(indx - 1);
+			Node np1 = GetNodeAt(indx); /* or use np1=nm1.next */
+			nm1.next = nn;
+			nn.next = np1;
+//	update summary
+			this.size++;
+		}
 	}
-	Node temp=this.head;
-	for(int i=1;i<=indx;i++) {
-		temp=temp.next;
+
+	public int RemoveFirst() throws Exception {
+		if (this.size == 0) {
+			throw new Exception("Linked list is empty");
+		}
+		int rv = this.head.data;
+		if (this.size == 1) {
+			this.head = null;
+			this.tail = null;
+			this.size = 0;
+		} else {
+			this.head = this.head.next;
+			this.size--;
+		}
+		return rv;
 	}
-	return temp.data;
-		
-}
-private Node GetNodeAt(int index)throws Exception {
-	if(this.size==0) {
-		throw new Exception("Linked list is empty");
+
+	public int RemoveLast() throws Exception {
+		if (this.size == 0) {
+			throw new Exception("Linked list is empty");
+		}
+		int rv = this.tail.data;
+		if (this.size == 1) {
+			this.head = null;
+			this.tail = null;
+			this.size = 0;
+		} else {
+			Node sizem2 = GetNodeAt(this.size - 2);
+			this.tail = sizem2;
+			this.tail.next = null;
+			this.size--;
+		}
+		return rv;
 	}
-	if(index<0||index>=this.size) {
-		throw new Exception("index is invalid");
+
+	public int RemoveAt(int indx) throws Exception {
+		if (indx < 0 || indx >= this.size)
+			throw new Exception("index is invalid");
+		if (indx == 0) {
+			return this.RemoveFirst();
+		} else if (indx == this.size) {
+			return this.RemoveLast();
+		} else {
+			Node nm1 = GetNodeAt(indx - 1);
+			Node n = nm1.next; /* or use int rv = GetAt(indx) and return rv */
+			Node np1 = GetNodeAt(indx + 1);
+			nm1.next = np1;
+			this.size--;
+			return n.data;
+		}
 	}
-	Node temp=this.head;
-	for(int i=1;i<=index;i++) {
-		temp=temp.next;
+
+	public void ReverseData() throws Exception {
+		int left = 0;
+		int right = size - 1;
+		while (left < right) {
+			Node ln = GetNodeAt(left);
+			Node rt = GetNodeAt(right);
+
+			int temp = ln.data;
+			ln.data = rt.data;
+			rt.data = temp;
+			left++;
+			right--;
+		}
 	}
-	return temp;
-}
-public void AddAt(int item,int indx)throws Exception {
-	if(indx<0||indx>this.size) {
-		throw new Exception("index is invalid");
+
+	public void ReversePointer() {
+		Node prev = this.head;
+		Node curr = prev.next;
+		while (curr != null) {
+			Node ahead = curr.next;
+			curr.next = prev;
+
+			prev = curr;
+			curr = ahead;
+		}
+//		Swap
+		Node t = this.head;
+		this.head = this.tail;
+		this.tail = t;
+
+		this.tail.next = null;
 	}
-	if(this.size==0) {
-		AddFirst(item);
+
+	public int Mid() {
+		Node slow = this.head;
+		Node fast = this.head;
+		while (fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow.data;
+
 	}
-	
-	else if(indx==this.size){
-		AddLast(item);
-	}else {
-	Node nn1=GetNodeAt(indx-1);
-	Node np1=nn1.next;
-//	craete new node
-	Node nn=new Node();
-	nn.data=item;
-	nn.next=null;
-//	atttach my new node at index
-	nn1.next=nn;
-	nn.next=np1;
-//	update summry
-	this.size++;
-	}
-}
-public int RemoveFirst() throws Exception{
-	if(this.size==0) {
-		throw new Exception("Linked list is empty");
-	}
-	int rv=this.head.data;
-	if(this.size==1) {
-		this.head=null;
-		this.tail=null;
-		this.size=0;
-	}
-	else {
-	this.head=this.head.next;
-	this.size--;
-	}
-	return rv;
-}
-public int RemoveLast() throws Exception{
-	if(this.size==0) {
-		throw new Exception("Linked list is empty");
-	}
-	int rv=this.tail.data;
-	if(this.size==1) {
-		this.head=null;
-		this.tail=null;
-		this.size=0;
-	}
-	else {
-		Node sizem2=GetNodeAt(this.size-1);
-	this.tail=sizem2;
-	this.tail.next=null;
-	this.size--;
-	}
-	return rv;
-}
-public int  RemoveAt(int indx) throws Exception {
-	if(indx<0||indx>=this.size)
-		throw new Exception("index is invalid");
-if(indx==0) {
-	return this.RemoveFirst();
-}
-else if(indx==this.size){
-	return this.RemoveLast();
-}
-else {
-	int rv=GetAt( indx);
-	Node np1=GetNodeAt(indx+1);
-	Node nm1=GetNodeAt(indx-1);
-	Node n=np1;
-	nm1.next=np1;
-	this.size--;
-	return rv;
-}
-} 
 }
